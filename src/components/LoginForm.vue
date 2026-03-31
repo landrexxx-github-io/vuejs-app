4dfbadb314b323bde3f8470028cfc44a3e188cb6
landrex <landrex.rebuera@gmail.com>
2026-03-31 10:41:26 +0800

    Initial commit

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { Button, FloatLabel, IconField, InputIcon, InputText } from 'primevue'
+import { ref } from 'vue'
+
+const username = ref('')
+const password = ref('')
+</script>
+<template>
+  <div class="min-h-screen flex justify-center items-start pt-16 px-4">
+    <div class="w-full max-w-md space-y-5">
+      <FloatLabel variant="in">
+        <IconField>
+          <InputIcon class="pi pi-user" />
+          <InputText
+            id="in_username"
+            v-model="username"
+            autocomplete="off"
+            variant="filled"
+            class="w-full"
+          />
+        </IconField>
+        <label for="in_username" class="font-light">Username</label>
+      </FloatLabel>
+      <FloatLabel variant="in">
+        <IconField>
+          <InputIcon class="pi pi-lock" />
+          <InputText
+            type="password"
+            id="in_password"
+            v-model="password"
+            autocomplete="off"
+            variant="filled"
+            class="w-full"
+          />
+        </IconField>
+        <label for="in_password" class="font-light">Password</label>
+      </FloatLabel>
+      <Button label="Login" class="w-full" size="large" />
+    </div>
+  </div>
+</template>
+<style scoped></style>
---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import PrimeVue from 'primevue/config'
+import '@fontsource/inter'
+import '@fontsource/inter/400.css'
+import '@fontsource/inter/500.css'
+import '@fontsource/inter/600.css'
+import '@fontsource/inter/700.css'
+
+import Aura from '@primeuix/themes/aura'
+// import Material from '@primeuix/themes/material'
+// import Lara from '@primeuix/themes/lara'
+// import Nora from '@primeuix/themes/nora'
+
+const app = createApp(App)
+
+app.use(router).use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+})
+
+app.mount('#app')
